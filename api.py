0a117d2a27969379ea99a149a5147f5dee8bc732
leila2361 <leilareille@gmail.com>
2021-11-28 22:13:09 +0100

    Ajout du radar et correction des bugs

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import os
-import sys
+
 
 from pickle import load
 import pandas as pd
@@ -7,7 +7,7 @@ import sklearn
 from flask import Flask, jsonify, request
 import json
 from sklearn.neighbors import NearestNeighbors
-from lightgbm import LGBMClassifier
+from sklearn.preprocessing import StandardScaler
 import shap
 
 
@@ -17,12 +17,16 @@ train_df = df_api[df_api.TARGET.notnull()]
 X_train = train_df[[col for col in train_df.columns if col != 'TARGET']]
 y_train = train_df ['TARGET']
 test_df = df_api[df_api.TARGET.isnull()]
-X_test = test_df[[col for col in test_df.columns if col != 'TARGET']]
+X_test = test_df.drop('TARGET', axis=1)
+path = os.path.join('data', 'X_tr_std.csv')
+X_tr_std = pd.read_csv(path, index_col='SK_ID_CURR')
+path = os.path.join('data', 'X_te_std.csv')
+X_te_std = pd.read_csv(path, index_col='SK_ID_CURR')
 
 path = os.path.join('data', 'feature_sel.csv')
 feat_sel = pd.read_csv(path, index_col=0)
-path = os.path.join('data', 'df_nn.csv')
-df_nn = pd.read_csv(path, index_col='SK_ID_CURR')
+important_features = list(feat_sel['feature'].values[:12])
+
 path = os.path.join('data', 'results.csv')
 results = pd.read_csv(path, index_col='SK_ID_CURR')
 # model
@@ -36,9 +40,7 @@ shap_val_all = pd.read_csv(path, index_col=0)
 path = os.path.join('data', 'expected_val.pkl')
 with open(path, 'rb') as file:
     expected_val = load(file)
-path = os.path.join('model', 'NearestNeighborsModel.pkl')
-with open(path, 'rb') as file:
-    nnm = load(file)
+
 
 #instantiate Flask object
 app = Flask(__name__)
@@ -62,8 +64,8 @@ def sk_ids():
 
 @app.route('/api/feat_imp/')
 def send_feat_imp():
-
-    feat_imp_json = json.loads(feat_sel.to_json())
+    feat_imp = pd.Series(feat_sel['importance'].values, index=feat_sel['feature'].values)
+    feat_imp_json = json.loads(feat_imp.to_json())
     # Return the processed data as a json object
     return jsonify({'status': 'ok',
     		        'data': feat_imp_json})
@@ -74,25 +76,56 @@ def data_cust():
     sk_id_cust = int(request.args.get('SK_ID_CURR'))
     # Get the personal data for the customer (pd.Series)
     X_cust_ser = X_test.loc[sk_id_cust, :]
+    X_cust_std = X_te_std.loc[sk_id_cust,:]
     # Convert the pd.Series (df row) of customer's data to JSON
     X_cust_json = json.loads(X_cust_ser.to_json())
+    X_cust_std_json = json.loads(X_cust_std.to_json())
     # Return the cleaned data
     return jsonify({'status': 'ok',
-    				'data': X_cust_json})
+    				'data': X_cust_json,
+                    'data_std': X_cust_std_json})
 
 # find 10 nearest neighbors among the training set
 def get_df_neigh(sk_id_cust):
     # get data of 10 nearest neigh in the X_train dataframe (pd.DataFrame)
+    df_neigh = X_tr_std[important_features].copy()
     neigh = NearestNeighbors(n_neighbors=10)
-    neigh.fit(X_train)
-    X_cust = X_test.loc[sk_id_cust: sk_id_cust]
-    idx = neigh.kneighbors(X=X_cust,
+    neigh.fit(df_neigh)
+    X_cust = X_te_std.loc[sk_id_cust: sk_id_cust]
+    cust_list = X_cust[important_features]
+    idx = neigh.kneighbors(X=cust_list,
                            n_neighbors=10,
                            return_distance=False).ravel()
-    nearest_cust_idx = list(X_train.iloc[idx].index)
-    X_neigh_df = X_train.loc[nearest_cust_idx, :]
+    nearest_cust_idx = list(X_tr_std.iloc[idx].index)
+    X_neigh_df = X_tr_std.loc[nearest_cust_idx, :]
     y_neigh = y_train.loc[nearest_cust_idx]
     return X_neigh_df, y_neigh
+
+def get_df_neigh_target(sk_id_cust):
+    # get data of 20 nearest neigh in the X_train dataframe (pd.DataFrame)
+    # 10 with loan granted and 10 with loan rejected
+    df_neigh = X_tr_std[important_features].copy()
+    df_neigh['TARGET'] = y_train
+    df_neigh0 = df_neigh[df_neigh['TARGET']==0.].copy()
+    df_neigh0.drop('TARGET',axis=1, inplace=True)
+    df_neigh1 = df_neigh[df_neigh['TARGET']==1.].copy()
+    df_neigh1.drop('TARGET',axis=1, inplace=True)
+    neigh = NearestNeighbors(n_neighbors=10)
+    neigh.fit(df_neigh0)
+    X_cust = X_te_std.loc[sk_id_cust: sk_id_cust]
+    cust_list = X_cust[important_features]
+    idx0 = neigh.kneighbors(X=cust_list,
+                           n_neighbors=10,
+                           return_distance=False).ravel()
+    nearest_cust_idx0 = list(X_tr_std.iloc[idx0].index)
+    X_neigh_0 = X_tr_std.loc[nearest_cust_idx0, :]
+    neigh.fit(df_neigh1)
+    idx1 = neigh.kneighbors(X=cust_list,
+                            n_neighbors=10,
+                           return_distance=False).ravel()
+    nearest_cust_idx1 = list(X_tr_std.iloc[idx1].index)
+    X_neigh_1 = X_tr_std.loc[nearest_cust_idx1, :]
+    return X_neigh_0, X_neigh_1
 
 @app.route('/api/neigh_cust/')
 def neigh_cust():
@@ -100,24 +133,31 @@ def neigh_cust():
     sk_id_cust = int(request.args.get('SK_ID_CURR'))
     # return the nearest neighbors
     X_neigh_df, y_neigh = get_df_neigh(sk_id_cust)
+    X_neigh_0, X_neigh_1 = get_df_neigh_target(sk_id_cust)
     # Convert the customer's data to JSON
     X_neigh_json = json.loads(X_neigh_df.to_json())
     y_neigh_json = json.loads(y_neigh.to_json())
+    X_neigh0_json = json.loads(X_neigh_0.to_json())
+    X_neigh1_json = json.loads(X_neigh_1.to_json())
     # Return the cleaned data jsonified
     return jsonify({'status': 'ok',
     				'X_neigh': X_neigh_json,
-    				'y_neigh': y_neigh_json})
+    				'y_neigh': y_neigh_json,
+                    'X_neigh0': X_neigh0_json,
+                    'X_neigh1': X_neigh1_json})
 
 @app.route('/api/all_proc_data_tr/')
 def all_proc_data_tr():
-    # get all data from X_tr_featsel, X_te_featsel and y_train data
+    # get all data from X_train, X_test and y_train data
     # and convert the data to JSON
-    X_train_json = json.loads(X_train.to_json())
+    X_tr_std_json = json.loads(X_tr_std.to_json())
     y_train_json = json.loads(y_train.to_json())
+    X_te_std_json = json.loads(X_te_std.to_json())
     # Return the cleaned data jsonified
     return jsonify({'status': 'ok',
-    				'X_train': X_train_json,
-    				'y_train': y_train_json})
+    				'X_tr_std': X_tr_std_json,
+    				'y_train': y_train_json,
+                   'X_te_std': X_te_std_json})
 
 
 
@@ -125,10 +165,8 @@ def all_proc_data_tr():
 def scoring_cust():
     # Parse http request to get arguments (sk_id_cust)
     sk_id_cust = int(request.args.get('SK_ID_CURR'))
-    # Get the data for the customer (pd.DataFrame)
-    X_cust = X_test.loc[sk_id_cust:sk_id_cust]
-	# Compute the score of the customer (using the whole pipeline)
-    proba_cust = results.loc[sk_id_cust:sk_id_cust]
+    # read the score of the customer
+    proba_cust = results.loc[sk_id_cust, 'Proba_impaye']
     # Return score
     return jsonify({'status': 'ok',
     		        'SK_ID_CURR': sk_id_cust,
@@ -159,7 +197,7 @@ def shap_values():
     sk_id_cust = int(request.args.get('SK_ID_CURR'))
     # return the nearest neighbors
     X_neigh, y_neigh = get_df_neigh(sk_id_cust)
-    X_cust = X_test.loc[sk_id_cust].to_frame(sk_id_cust).T
+    X_cust = X_te_std.loc[sk_id_cust].to_frame(sk_id_cust).T
     X_neigh_ = pd.concat([X_neigh, X_cust], axis=0)
     # prepare the shap values of nearest neighbors + customer
     shap_val_neigh_ = shap_val_all.loc[X_neigh_.index]
