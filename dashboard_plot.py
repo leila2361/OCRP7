87c1a86208bbddf06130e988c68f3b85a31de4c0
leila2361 <leilareille@gmail.com>
2021-11-28 22:23:11 +0100

    ajout de la fonction radar

--- dashboard_plot.py
+++ dashboard_plot.py
@@ -24,7 +24,7 @@ def boxplot_var_by_target(X_all,y_all, X_neigh, y_neigh, X_cust, main_cols, figs
                 width=0.4, palette=['tab:green', 'tab:red'], showfliers=False, saturation=0.5,
                 ax=ax)
 
-    # 20 nearest neighbors
+    # 10 nearest neighbors
     df_melt_neigh = df_neigh.reset_index()
     df_melt_neigh.columns = ['index'] + list(df_melt_neigh.columns)[1:]
     df_melt_neigh = df_melt_neigh.melt(id_vars=['index', 'TARGET'],  # SK_ID_CURR
@@ -32,16 +32,140 @@ def boxplot_var_by_target(X_all,y_all, X_neigh, y_neigh, X_cust, main_cols, figs
                                        var_name="variables",
                                        value_name="values")
     sns.swarmplot(data=df_melt_neigh, x='variables', y='values', hue='TARGET', linewidth=1,
-                  palette=['darkgreen', 'darkred'], marker='o', edgecolor='k', ax=ax)
+                  palette=['green', 'red'], marker='o', edgecolor='k', ax=ax)
 
     # applicant customer
     df_melt_cust = df_cust.rename(columns={'index': "variables"})
     sns.swarmplot(data=df_melt_cust, x='variables', y='values', linewidth=1, color='y',
-                  marker='o', size=10, edgecolor='k', label='applicant customer', ax=ax)
+                  marker='X', size=10, edgecolor='k', label='applicant customer', ax=ax)
 
     # legend
     h, _ = ax.get_legend_handles_labels()
-    ax.legend(handles=h[:5])
+    #ax.legend(handles=h[:5])
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+    ax.legend(handles=h[:5], loc='center right', bbox_to_anchor=(1.6, 0.5), ncol=1)
 
     plt.xticks(rotation=20, ha='right')
+    plt.show()
+
+
+def radar_neigh(client):
+    """Radar of 6 main features for cust and 20 nearest neighbors (10 loan granted, 10 loan rejected)"""
+
+    def _invert(x, limits):
+        """inverts a value x on a scale from
+        limits[0] to limits[1]"""
+        return limits[1] - (x - limits[0])
+
+    def _scale_data(data, ranges):
+        """scales data[1:] to ranges[0],
+        inverts if the scale is reversed"""
+        for d, (y1, y2) in zip(data, ranges):
+            assert (y1 <= d <= y2) or (y2 <= d <= y1)
+
+        x1, x2 = ranges[0]
+        d = data[0]
+
+        if x1 > x2:
+            d = _invert(d, (x1, x2))
+            x1, x2 = x2, x1
+
+        sdata = [d]
+
+        for d, (y1, y2) in zip(data[1:], ranges[1:]):
+            if y1 > y2:
+                d = _invert(d, (y1, y2))
+                y1, y2 = y2, y1
+
+            sdata.append((d - y1) / (y2 - y1) * (x2 - x1) + x1)
+
+        return sdata
+
+    class ComplexRadar():
+        def __init__(self, fig, variables, ranges,
+                     n_ordinate_levels=6):
+            angles = np.arange(0, 360, (360. / len(variables)))
+
+            axes = [fig.add_axes([0.1, 0.1, 0.9, 0.9], polar=True,
+                                 label="axes{}".format(i))
+                    for i in range(len(variables))]
+
+            axes[0].set_thetagrids(angles, labels=[])
+
+            for ax in axes[1:]:
+                ax.patch.set_visible(False)
+                ax.grid("off")
+                ax.xaxis.set_visible(False)
+
+            for i, ax in enumerate(axes):
+                grid = np.linspace(*ranges[i],
+                                   num=n_ordinate_levels)
+                gridlabel = ["{}".format(round(x, 2))
+                             for x in grid]
+                if ranges[i][0] > ranges[i][1]:
+                    grid = grid[::-1]  # hack to invert grid
+                    # gridlabels aren't reversed
+                gridlabel[0] = ""  # clean up origin
+                ax.set_rgrids(grid, labels=gridlabel, angle=angles[i])
+                # ax.spines["polar"].set_visible(False)
+                ax.set_ylim(*ranges[i])
+
+            ticks = angles
+            ax.set_xticks(np.deg2rad(ticks))  # crée les axes suivant les angles, en radians
+            ticklabels = variables
+            ax.set_xticklabels(ticklabels, fontsize=10)  # définit les labels
+
+            angles1 = np.linspace(0, 2 * np.pi, len(ax.get_xticklabels()) + 1)
+            angles1[np.cos(angles1) < 0] = angles1[np.cos(angles1) < 0] + np.pi
+            angles1 = np.rad2deg(angles1)
+            labels = []
+            for label, angle in zip(ax.get_xticklabels(), angles1):
+                x, y = label.get_position()
+                lab = ax.text(x, y - .5, label.get_text(), transform=label.get_transform(),
+                              ha=label.get_ha(), va=label.get_va())
+                lab.set_rotation(angle)
+                lab.set_fontsize(12)
+                lab.set_fontweight('bold')
+                labels.append(lab)
+            ax.set_xticklabels([])
+
+            # variables for plotting
+            self.angle = np.deg2rad(np.r_[angles, angles[0]])
+            self.ranges = ranges
+            self.ax = axes[0]
+
+        def plot(self, data, *args, **kw):
+            sdata = _scale_data(data, self.ranges)
+            self.ax.plot(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
+
+        def fill(self, data, *args, **kw):
+            sdata = _scale_data(data, self.ranges)
+            self.ax.fill(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
+
+    # data
+    variables = ("Mean_Ext_Source", "PAYMENT_RATE", "DAYS_EMPLOYED",
+                 "AMT_ANNUITY", "DAYS_BIRTH", "INSTAL_DPD_MEAN")
+    data_ex = client.iloc[0]
+    ranges = [(min(client["Mean_Ext_Source"]) - 0.2, max(client["Mean_Ext_Source"]) + 0.2),
+              (min(client["PAYMENT_RATE"]) - 0.2, max(client["PAYMENT_RATE"]) + 0.2),
+              (min(client["DAYS_EMPLOYED"]) - 0.2, max(client["DAYS_EMPLOYED"]) + 0.2),
+              (min(client["AMT_ANNUITY"]) - 0.2, max(client["AMT_ANNUITY"]) + 0.2),
+              (min(client["DAYS_BIRTH"]) - 0.2, max(client["DAYS_BIRTH"]) + 0.2),
+              (min(client["INSTAL_DPD_MEAN"]) - 0.2, max(client["INSTAL_DPD_MEAN"]) + 0.2)]
+    # plotting
+    fig1 = plt.figure(figsize=(6, 6))
+    radar = ComplexRadar(fig1, variables, ranges)
+    radar.plot(data_ex, label='Cust')
+    radar.fill(data_ex, alpha=0.2)
+
+    radar.plot(client.iloc[1],
+               label='Mean of neighbohrs loan granted',
+               color='g')
+    radar.plot(client.iloc[2],
+               label='Mean of neighbohrs loan rejected',
+               color='r')
+
+    fig1.legend(bbox_to_anchor=(1.7, 1))
+
     plt.show()
